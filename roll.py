34c02b8f5859727c409843435404ad9fc4a0053a
Erin Morelli <erin@erinmorelli.com>
2015-08-10 16:03:21 -0400

    Adding new files for heroku

---
+++ roll.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# EM Slack Roll
+# Copyright (c) 2015 Erin Morelli
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+''' Run the EM Slack Roll application
+'''
+
+from os import environ
+from slack_roll.app import APP
+
+if __name__ == '__main__':
+    port = int(environ.get("PORT", 5000))
+    APP.run(host='0.0.0.0', port=port)
+
